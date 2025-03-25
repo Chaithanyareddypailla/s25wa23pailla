98e24cf0eb976064c9251d991ae9ce928cb1257b
Chaithanyareddypailla <chaithanyareddypailla@gmail.com>
2025-03-25 13:24:25 -0500

    app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var mydataRouter = require('./routes/mydata');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/computation', computationRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
