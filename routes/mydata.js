7969f7589cd21bd3c73ed7971ca993614fb3e3e1
Chaithanyareddypailla <chaithanyareddypailla@gmail.com>
2025-03-25 12:34:38 -0500

    routes

--- routes/mydata.js
+++ routes/mydata.js
@@ -1,10 +1,10 @@
-var express = require('Chaithanya Reddy Pailla');
+var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET mydata page. */
 router.get('/', function(req, res, next) {
   res.render('mydata', { title: 'Chaithanya Reddy Pailla' });
-
 });
 
 module.exports = router;
+
